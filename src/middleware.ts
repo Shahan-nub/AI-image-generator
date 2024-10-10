741500a70da58b0b707a9e012fdeb90f6ab2595e
Shahan-nub <shahananwer786@gmail.com>
2024-10-10 20:50:25 +0530

    jjjj

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/api(.*)'])
+// const isProtectedRoute = createRouteMatcher(['/api(.*)'])
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-})
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect()
+// })
+
+export default clerkMiddleware();
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
