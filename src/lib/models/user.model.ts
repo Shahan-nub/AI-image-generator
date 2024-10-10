d2faa1f654d0c94bc87000762b032e8aae861bf0
Shahan-nub <shahananwer786@gmail.com>
2024-10-10 20:11:51 +0530

    jj

--- src/lib/models/user.model.ts
+++ src/lib/models/user.model.ts
@@ -11,7 +11,7 @@ const UserSchema = new Schema({
         required:true,
         unique:true,
     },
-    usename:{
+    username:{
         type:String,
         required:true,
         unique:true,
